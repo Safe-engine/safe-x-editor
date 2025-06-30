45bb3ececa428a3b0dfafc411401ea5c2c4560f4
antn1 <antn@ftech.com.vn>
2025-06-30 14:49:01 +0700

    fix html index path

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -3,18 +3,17 @@ import Router from './router/Router';
 import { getEditorWebview } from './webview';
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log("Activated safexEditor")
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'safexEditor.openSafex',
       async (uri: vscode.Uri) => {
-
         const panel = vscode.window.createWebviewPanel(
           'safexEditor',
           'Safex Editor',
           vscode.ViewColumn.One,
           { enableScripts: true }
         );
-
         panel.webview.html = getEditorWebview(context, uri.fsPath);
         Router(panel, context)
       }
--- vscode/src/webview.ts
+++ vscode/src/webview.ts
@@ -7,7 +7,7 @@ export function getEditorWebview(context: vscode.ExtensionContext, filePath: str
   // const indexHtml = path.join(context.extensionPath, 'media', 'index.html')
   const folders = vscode.workspace.workspaceFolders;
   const rootProject = folders[0].uri.fsPath
-  const indexHtml = path.join(context.extensionPath, '..', 'dist', 'index.html')
+  const indexHtml = path.join(context.extensionPath, 'media', 'index.html')
   const html = readFileSync(indexHtml, 'utf-8')
   return html.replace('REPLACE_FILE_PATH', filePath).replace('REPLACE_ROOT_PROJECT', rootProject)
 }
