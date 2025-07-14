a5af2d5e150dc396f949d876556c7b0e37f70071
antn1 <antn@ftech.com.vn>
2025-07-14 13:52:27 +0700

    fix middlewares

--- src/states/app.middleware.ts
+++ src/states/app.middleware.ts
@@ -1,5 +1,35 @@
+import { sendRequest } from 'app/app.ipc';
+import { setLastRootFolder } from 'data/AppData';
 import { Dispatch } from 'react';
-import { createMiddleware } from './actions';
+import toast from 'react-hot-toast';
+import { GET_FOLDER_FILES, LOAD_COMPONENT_REQUEST } from 'shared/constant.message';
+import { Actions, createActions } from './actions';
+
+export function createMiddleware(dispatch: Dispatch<any>) {
+  const { getFilesSuccess, loadComponentSuccess } = createActions(dispatch)
+  const middlewares: Partial<Actions> = {
+    async getFiles(src: string) {
+      const data: any = await sendRequest({
+        key: GET_FOLDER_FILES,
+        src
+      });
+      if (data.error) {
+        toast.error(data.message);
+        return;
+      }
+      setLastRootFolder(src);
+      getFilesSuccess(data);
+    },
+    async loadComponent(path: string) {
+      const data: any = await sendRequest({
+        key: LOAD_COMPONENT_REQUEST,
+        path
+      });
+      loadComponentSuccess(data);
+    },
+  }
+  return middlewares
+}
 
 export const applyMiddleware =
   (dispatch: Dispatch<any>) => async (action: any) => {
@@ -7,5 +37,4 @@ export const applyMiddleware =
     if (middlewares[action.type]) {
       middlewares[action.type](...action.data);
     }
-    dispatch(action);
   };
